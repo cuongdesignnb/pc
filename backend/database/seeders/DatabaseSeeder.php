53d43710fac202a5aaa414b9708d957d2784f718
cuongdesign <dinhcuongdesign@gmail.com>
2026-09-05 14:45:00 +0700

    feat: add homepage merchandising assets

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -27,6 +27,7 @@ class DatabaseSeeder extends Seeder
             PostSeeder::class,
             SettingSeeder::class,
             ImageSeeder::class,
+            HomepageMerchandisingSeeder::class,
             HomepageReferenceSeeder::class,
             ReferenceProductDetailSeeder::class,
         ]);
